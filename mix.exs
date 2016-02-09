11638b241bf9e1f01092e3dccbbe61671e302f60
Aleksei Magusev <lexmag@me.com>
2016-02-09 12:37:41 +0100

    Bring Travis CI

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Dicon.Mixfile do
   def project() do
     [app: :dicon,
      version: "0.0.1",
-     elixir: "~> 1.2",
+     elixir: "~> 1.1",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod]
   end
