1ba2985bcd001b7aae9862974f14c99ab3937c87
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2017-03-27 15:35:29 +0200

    Fix charlist warnings and bump Elixir requirement (#28)

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Dicon.Mixfile do
   def project() do
     [app: :dicon,
      version: "0.4.0",
-     elixir: "~> 1.2",
+     elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      package: package(),
