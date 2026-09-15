612dfb916e8187c8604f91d998ddf352cc7c033f
Aleksei Magusev <lexmag@me.com>
2026-09-15 02:01:10 -0700

    Drop support for Elixir versions before 1.12

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Dicon.Mixfile do
     [
       app: :dicon,
       version: "0.5.0",
-      elixir: "~> 1.3",
+      elixir: "~> 1.12",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       package: package(),
